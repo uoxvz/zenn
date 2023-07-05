032ecab2f271166a733877d9c335c8a922ebb56d
uoxvz <matsukumatatsuhiko@gmail.com>
2023-07-05 15:04:01 +0900

    refile s3 再設定

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,14 @@
-require "refile/s3"
+require 'refile/s3'
 
-aws = {
-    access_key_id: ENV["AKIAZ2I7KVX2O7K3V3XQ"],
-    secret_access_key: ENV["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"],
-    region: "ap-northeast-1",
-    bucket: "heroku-rails-refile-2023",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+#if !Rails.env.production? # 開発環境でS3へアップロードできているか確認する方法
+
+if Rails.env.production? # 本番環境の場合
+    aws = {
+    access_key_id: ENV['AKIAZ2I7KVX2GMZPOLWJ'], # アクセスキーID
+    secret_access_key: ENV['jT3n25xSISvSOfERIiGJ5cvs/hKvTNCRD3qsTshw'], # シークレットアクセスキー
+    region: 'ap-northeast-1', # リージョン
+    bucket: 'heloku-image',
+    }
+    Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+    Refile.store = Refile::S3.new(prefix: 'store', **aws)
+end

c07f8cbb7dff9beb3f2b0f29ed34602049fe8275
uoxvz <matsukumatatsuhiko@gmail.com>
2023-07-05 16:30:39 +0900

    commit s3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,10 +4,10 @@ require 'refile/s3'
 
 if Rails.env.production? # 本番環境の場合
     aws = {
-    access_key_id: ENV['AKIAZ2I7KVX2GMZPOLWJ'], # アクセスキーID
-    secret_access_key: ENV['jT3n25xSISvSOfERIiGJ5cvs/hKvTNCRD3qsTshw'], # シークレットアクセスキー
+    access_key_id: ENV['AKIAZ2I7KVX2MLVIABWK'], # アクセスキーID
+    secret_access_key: ENV['9uOKASi5AT5AYaVyZUpOaUgGh1eKt32d4APh0jn2'], # シークレットアクセスキー
     region: 'ap-northeast-1', # リージョン
-    bucket: 'heloku-image',
+    bucket: 'heroku-uoxvz',
     }
     Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
     Refile.store = Refile::S3.new(prefix: 'store', **aws)
